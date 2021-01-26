7af6ca4cce4e7fdb7dbe2722c0bde977fc7760b3
Boderio <boderiotrigan@hotmail.com>
2021-01-26 10:14:57 +0100

    markdown v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import marked from 'marked'
+import DOMPurify from 'dompurify'
+
+import { sampleText } from './sampleText'
+
+class App extends Component {
+  state = {
+    text : sampleText
+  }
+
+  handleChange = (event) => {
+    const text = event.target.value 
+    //this.setState({text:text})
+    this.setState({text})
+  }
+
+  renderText = text => {
+    let text2 = DOMPurify.sanitize(text,{ALLOWED_TAGS:['b']})
+    return marked(text2)
+  }
+
+  render() { 
+    return ( 
+      <>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <textarea 
+              rows="35"
+              value={this.state.text}
+              className="form-control"
+              onChange={this.handleChange}
+              ></textarea>
+            </div>
+            <div className="col-sm-6">
+              <div dangerouslySetInnerHTML={{__html: this.renderText(this.state.text)}}></div>
+            </div>
+          </div>
+        </div>
+      </>
+     );
+  }
 }
-
+ 
 export default App;
+
